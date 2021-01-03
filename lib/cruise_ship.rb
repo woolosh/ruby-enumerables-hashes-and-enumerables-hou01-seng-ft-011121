df7489eba6026222d8ef0164c76e823edc156632
Joshua Morales <joshuapmorales@gmail.com>
2021-01-03 19:14:40 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -12,8 +12,7 @@ def select_winner(passengers)
   
   passengers.each do |suite, name|
     
-    if suite == :suite_a &&
-      name.start_with?("A")
+    if suite == :suite_a && name.start_with?("A")
       
       winner = name
       
