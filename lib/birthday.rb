f862837f84b93c12ff7647110e8011c620b2148b
Joshua Morales <joshuapmorales@gmail.com>
2021-01-03 19:54:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,7 +10,7 @@ def happy_birthday(birthday_kids)
   def happy_birthday(birthday_kids)
     
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday {kids_name}! You are now {age} years old!"
     
   end
   
